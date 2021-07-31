32eed210bf5e39bbf58bf22f5f51487ddf41f4fd
afonso mateus <afonsoprad10@gmail.com>
2021-07-31 14:08:06 -0400

    payload of product request

---
+++ src/main/java/br/com/poc/dynamodb/productmanagement/ProductRequest.java
@@ -0,0 +1,42 @@
+package br.com.poc.dynamodb.productmanagement;
+
+import java.math.BigDecimal;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+
+public class ProductRequest {
+
+	@NotBlank
+	private String name;
+	@NotBlank
+	@Size(max = 500)
+	private String description;
+	@PositiveOrZero
+	private Integer stock;
+	@NotNull
+	@Positive
+	private BigDecimal price;
+
+	@Deprecated
+	public ProductRequest() {}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public Integer getStock() {
+		return this.stock;
+	}
+
+	public BigDecimal getPrice() {
+		return this.price;
+	}
+
+}
