249e47d68651ad9ba116c11d6e51f3f2a728af8e
afonso mateus <afonsoprad10@gmail.com>
2021-07-31 13:54:41 -0400

    setup project

---
+++ src/test/java/br/com/poc/dynamodb/DynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.poc.dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
