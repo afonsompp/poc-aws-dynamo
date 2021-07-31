99036a92a33a4bc2608d1995d442067cef1d8da3
afonso mateus <afonsoprad10@gmail.com>
2021-07-31 14:19:22 -0400

    create repository of product entity

---
+++ src/main/java/br/com/poc/dynamodb/productmanagement/ProductRepository.java
@@ -0,0 +1,8 @@
+package br.com.poc.dynamodb.productmanagement;
+
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface ProductRepository extends CrudRepository<Product, String> {
+}
